120c5714997f2dc773c61d15ee9f3dcb1eceb182
SubhradeepBasu18 <subhradeepbasu2002@gmail.com>
2025-10-14 20:48:11 +0530

    Fixed Multer Bugs

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -27,12 +27,12 @@ export const upload = multer({
     limits: {
         fileSize: 5 * 1024 * 1024, // 5MB limit
     },
-    fileFilter: (req, file, cb) => {
-        // Check if file is an image
-        if (file.mimetype.startsWith('image/')) {
-            cb(null, true);
-        } else {
-            cb(new Error('Only image files are allowed!'), false);
-        }
-    }
+    // fileFilter: (req, file, cb) => {
+    //     // Check if file is an image
+    //     if (file.mimetype.startsWith('image/')) {
+    //         cb(null, true);
+    //     } else {
+    //         cb(new Error('Only image files are allowed!'), false);
+    //     }
+    // }
 });
